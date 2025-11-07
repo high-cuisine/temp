f05d7cf0687cccec4da2f6cbab2e6cb0be85eda1
Александр Носиков <aleksandrnosikov@MacBook-Pro-Aleksandr.local>
2025-11-07 18:39:56 +0400

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import * as dotenv from 'dotenv';
 dotenv.config({ path: '.env' });
 
+import { randomUUID } from 'crypto';
+
+if (!(global as any).crypto) {
+  (global as any).crypto = {};
+}
+
+if (typeof (global as any).crypto.randomUUID !== 'function') {
+  (global as any).crypto.randomUUID = randomUUID;
+}
+
 import('./boostrap').then(({ bootstrap }) => {
   bootstrap();
 });
